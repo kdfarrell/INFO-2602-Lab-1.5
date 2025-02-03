627d5f2aa0285845a36feef61e458b72f504a778
Kareem Farrell <kareemfarrellonline@gmail.com>
2025-02-03 14:38:46 -0400

    Updated \studets route added id pref

--- main.py
+++ main.py
@@ -14,10 +14,23 @@ with open('data.json') as f:
 def hello_world():
     return 'Hello, World!'  # return 'Hello World' in response
 
+@app.route('/students')
+def get_students() :
+    result = []
+    pref = request.args.get('pref') # get the parameter from url
+    if pref:
+        for student in data: # iterate dataset
+          if student['pref'] == pref: # select the students with a given meal preference
+            result.append(student) # add match student to the result
+        return jsonify(result) # return filtered set if parameter is supplied
+    return jsonify(data) # return entire dataset if no parameter supplied
+
+# route variables
 @app.route('/students/<id>')
 def get_student(id):
   for student in data: 
     if student['id'] == id: # filter out the students without the specified id
       return jsonify(student)
-
+  return f"Student {id} does not exist"
+            
 app.run(host='0.0.0.0', port=8080, debug=True)
