ed9dc8f98a3208e90838f36f3631846aa1f43dad
Kareem Farrell <kareemfarrellonline@gmail.com>
2025-02-03 14:37:06 -0400

    Added \students route

--- main.py
+++ main.py
@@ -14,4 +14,10 @@ with open('data.json') as f:
 def hello_world():
     return 'Hello, World!'  # return 'Hello World' in response
 
+@app.route('/students/<id>')
+def get_student(id):
+  for student in data: 
+    if student['id'] == id: # filter out the students without the specified id
+      return jsonify(student)
+
 app.run(host='0.0.0.0', port=8080, debug=True)
